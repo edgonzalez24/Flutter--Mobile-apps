c92a8b4966b0dda748a933cd5f064ddadfaf4021
Tux G. <edgoca2498@gmail.com>
2026-09-18 13:18:41 -0600

    feat: Add route names for HomeScreen, ButtonsScreen, and CardsScreen

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:widgets_app/presentation/widgets/home/home_view_widget.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+  
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Widgets + Material 3'),
-      ),
+      appBar: AppBar(title: const Text('Widgets + Material 3')),
       body: const HomeViewWidget(),
     );
   }
